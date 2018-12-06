6e5f9bdf9faef5be53c599850b11f3c569f8d357
zhangkunhua <zhangkunhua@le.com>
2018-12-06 22:34:22 +0800

    es6 1206

--- es6/app/js/class/lesson1.js
+++ es6/app/js/class/lesson1.js
@@ -1,4 +1,3 @@
-
 /*
 	let  var const
  */
@@ -6,14 +5,13 @@ function test() {
 	let a = 1;
 	console.log(a);
 }
-
 /*
 	1.实验证明，当常量是数组或对象时，可以修改const的，但是不能修改地址
 	2.必须在声明时候赋值，否则报错
 
  */
 function changeConst() {
-	const arr = [1,2,3];
+	const arr = [1, 2, 3];
 	arr[1] = 4;
 	// arr = [4,5,6];
 
@@ -21,24 +19,28 @@ function changeConst() {
 		name: "zhangsan"
 	}
 
-	obj.age =12;
+	obj.age = 12;
 	console.log(obj);
 
 
-	const  variable = "string";
+	const variable = "string";
 	// variable = "boolean";
 	console.log(variable);
 }
 
-function f() { console.log('I am outside!'); }
+function f() {
+	console.log('I am outside!');
+}
 
-(function () {
-  if (false) {
-    // 重复声明一次函数f
-    function f() { console.log('I am inside!'); }
-  }
+(function() {
+	if (false) {
+		// 重复声明一次函数f
+		function f() {
+			console.log('I am inside!');
+		}
+	}
 
-  f();
+	f();
 }());
 
 changeConst();
--- es6/app/js/class/lesson7.js
+++ es6/app/js/class/lesson7.js
@@ -1,8 +1,8 @@
 /*
- es6数组学习
+ es6数组扩展
 
- Array.of 数值变数组
- Array.from 类似数组的对象（array-like object）和可遍历（iterable）的对象（包括 ES6 新增的数据结构 Set 和 Map）。
+Array.of 数值变数组
+Array.from 类似数组的对象（array-like object）和可遍历（iterable）的对象（包括 ES6 新增的数据结构 Set 和 Map）。
 Array.from还可以接受第二个参数，作用类似于数组的map方法，用来对每个元素进行处理，将处理后的值放入返回的数组。
 
  */
